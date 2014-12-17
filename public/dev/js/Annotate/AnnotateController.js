41e402dbd20b2cc329f36489ced85c6f1744f9b2
Joseph O'Reilly <zebone29@gmail.com>
2014-12-17 15:40:07 +0000

    annotation marks added

--- public/dev/js/Annotate/AnnotateController.js
+++ public/dev/js/Annotate/AnnotateController.js
@@ -11,6 +11,18 @@
         _this = this;
         _this.design = {};
 
+        // settings for showing messages and what not
+        _this.openComment = false;
+        _this.commentSelected = null;
+
+        _this.settings = {};
+
+        _this.height = angular.element(document.querySelector('.design-img'))[0].clientHeight;
+
+        console.log(_this.height);
+
+        _this.newAnnotation = null;
+
         _this.getDesign=function(){
             // console.log(SingProjFactory);
             AnnotateFactory.getDesign()
@@ -27,18 +39,44 @@
         }
 
         _this.annotate = function(e) {
+
             var mouse = getMouse(e);
-            console.log(_this.design.annotations);
-            if(_this.design.annotations){
-                var mark = {};
-                mark.circle = {};
-                mark.circle.x = mouse.x;
-                mark.circle.y = mouse.y;
-                mark.circle.color = "#000";
-                _this.design.annotations.push(mark);
-            }
-            console.log(mouse);
+
+            var newAnnote = {};
+
+            newAnnote.circle = {};
+
+            newAnnote.circle.x = mouse.x;
+            newAnnote.circle.y = mouse.y;
+            newAnnote.circle.color = '#000';
+
+            _this.newAnnotation = newAnnote;
         };
+
+        _this.toggleComments = function (index) {
+
+            _this.commentSelected = index;
+
+            _this.openComment = !_this.openComment;
+        }
+
+
+        _this.checkCoords = function (coord, value) {
+            return coord > value;
+        }
+
+
+        // console.log(_this.design.annotations);
+        // if(_this.design.annotations){
+        //     var mark = {};
+        //     mark.circle = {};
+        //     mark.circle.x = mouse.x;
+        //     mark.circle.y = mouse.y;
+        //     mark.circle.color = "#000";
+        //     //
+        //     _this.design.annotations.push(mark);
+        // }
+        // console.log(mouse);
 
 
 
